efcd5a49b6289bbd476163a135ef73235493aaec
Danny Povolotski <dannypovolotski@gmail.com>
2021-10-15 02:11:13 +0300

    Fix browser error (#23)

--- src/utils/bytes32.ts
+++ src/utils/bytes32.ts
@@ -2,7 +2,7 @@ import invariant from "tiny-invariant";
 
 export const toBytes32Array = (b: Buffer): number[] => {
   invariant(b.length <= 32, `invalid length ${b.length}`);
-  const buf = new Uint8Array(Buffer.alloc(32));
+  const buf = Buffer.alloc(32);
   b.copy(buf, 32 - b.length);
 
   return Array.from(buf);
