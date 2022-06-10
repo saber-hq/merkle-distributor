ddab4c30272a3fb7f2dbb8c31f0c99f30315b998
Ian Macalinao <ian@saber.so>
2022-06-10 14:32:21 -0500

    Upgrade dependencies (#323)
    * Upgrade dependencies
    
    * cjs
    
    * add exports to package.json
    
    * peer dep bump

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,10 @@
+// @ts-check
+
+"use strict";
+
 require("@rushstack/eslint-patch/modern-module-resolution");
 
+/** @type import('eslint').Linter.Config */
 module.exports = {
   extends: ["@saberhq"],
   parserOptions: {
